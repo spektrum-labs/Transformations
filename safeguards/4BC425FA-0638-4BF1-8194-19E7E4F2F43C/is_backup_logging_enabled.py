a76e646825f3d3495f22c73b9ee6cafef7a33dcc
Tim Butler <tim@spektrum.ai>
2025-05-06 13:03:42 -0400

    Adding additional vendor

--- safeguards/4BC425FA-0638-4BF1-8194-19E7E4F2F43C/is_backup_logging_enabled.py
+++ safeguards/4BC425FA-0638-4BF1-8194-19E7E4F2F43C/is_backup_logging_enabled.py
@@ -17,10 +17,48 @@ def transform(input):
                 return input
             raise ValueError("Input must be JSON string, bytes, or dict")
     
-        data = _parse_input(input).get("response", {}).get("result", _parse_input(input))
+        # Ensure input is a dictionary by parsing if necessary
+        input = _parse_input(input)
+
+        # Extract response safely
+        input = input.get("response",input)
+        input = input.get("result",input)
+
+        if 'dbBackups' in input:
+            dbBackups = input.get("dbBackups",input)
+        
+        if 'dbManualSnapshots' in input:
+            dbManualSnapshots = input.get("dbManualSnapshots",input)
+        
+        if 'volumeSnapshots' in input:
+            volumeSnapshots = input.get("volumeSnapshots",input)
+            
+        #Check for Automated DB Backups
+        response = dbBackups.get("DescribeDBInstanceAutomatedBackupsResponse", {})
+        result = response.get("DescribeDBInstanceAutomatedBackupsResult", {})
+        automated_backups = result.get("DBInstanceAutomatedBackups", [])
+
+        # Count total backups
+        if isinstance(automated_backups, dict):
+            automated_backups = [automated_backups]
+            
+        total_db_backups = len(automated_backups)
+
+        #Check for Manual DB Backups
+        response = dbManualSnapshots.get("DescribeDBSnapshotsResponse", {})
+        result = response.get("DescribeDBSnapshotsResult", {})
+        manual_backups = result.get("DBSnapshots", {})
+        manual_backups = manual_backups.get("DBSnapshot", [])
+        
+        # Count total backups
+        if isinstance(manual_backups, dict):
+            manual_backups = [manual_backups]
+            
+        total_db_backups += len(manual_backups)
 
         # No restore/test records in this payload → always False
-        logging_enabled = bool(data.get("restoreJobs", []))
+        logging_enabled = True if total_db_backups > 0 else False
+        
         return {"isBackupLoggingEnabled": logging_enabled}
 
     except json.JSONDecodeError:
