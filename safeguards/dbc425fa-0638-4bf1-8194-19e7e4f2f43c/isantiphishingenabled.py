f0dc38a3b83e978cb5ba1498bd6277a413c21201
Tim Butler <tim@spektrum.ai>
2025-07-31 10:56:13 -0400

    Add overall Security Enabled

--- safeguards/dbc425fa-0638-4bf1-8194-19e7e4f2f43c/isantiphishingenabled.py
+++ safeguards/dbc425fa-0638-4bf1-8194-19e7e4f2f43c/isantiphishingenabled.py
@@ -1,12 +1,12 @@
 def transform(input):
     """
-    Evaluates the MFA status for  given IDP
+    Evaluates Mail Policies in place to check for Anti-Phishing settings
 
     Parameters:
-        input (dict): The JSON data containing IDP information.
+        input (dict): The JSON data containing Mail Policies information.
 
     Returns:
-        dict: A dictionary summarizing the MFA information.
+        dict: A dictionary summarizing the Mail Policies information.
     """
 
     try:
@@ -22,18 +22,26 @@ def transform(input):
             if any(key in policy["setting"]["value"] for key in filter_attributes)
         ]
 
-        isAntiPhishingEnabled = True
+        isAntiPhishingEnabled = False
+        isEmailSecurityEnabled = False
+
+        if len(input["policies"]) > 0:
+            isEmailSecurityEnabled = True
+
+        # If any of the matching values are None or False, set isAntiPhishingEnabled to False
         if len(matching_values) > 0:
+            isAntiPhishingEnabled = True
             for key,value in matching_values:
                 if value is None or not bool(value):
                     isAntiPhishingEnabled = False
                     break
 
         policy_info = {
+            "isEmailSecurityEnabled": isEmailSecurityEnabled,
             "isAntiPhishingEnabled": isAntiPhishingEnabled,
             "policyDetails": matching_values
         }
         return policy_info
     except Exception as e:
-        return {"isAntiPhishingEnabled": False, "error": str(e)}
+        return {"isEmailSecurityEnabled": False, "isAntiPhishingEnabled": False, "error": str(e)}
         
