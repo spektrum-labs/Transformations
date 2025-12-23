99c1f0ef44f2cdbf4c38bdc46c40f1c181d0fc4e
Tim Butler <tim@spektrum.ai>
2025-12-23 18:26:32 -0500

    Return all properties for evaluation for temporary access

--- safeguards/d9b6f27a-2e67-4b55-a09e-0784c5de9abd/auth_types_allowed.py
+++ safeguards/d9b6f27a-2e67-4b55-a09e-0784c5de9abd/auth_types_allowed.py
@@ -15,12 +15,12 @@ def transform(input):
             input = input['response']
             
         if 'authenticationMethodConfigurations' in input:
-            mfa_enrolled = [{"id": obj['id'] if 'id' in obj else '', "state": obj['state'] if 'state' in obj else 'enabled', "includeTargets": obj['includeTargets'] if 'includeTargets' in obj else []} for obj in input['authenticationMethodConfigurations'] if 'state' in obj and str(obj['state']).lower() == "enabled"]
+            mfa_enrolled = [obj for obj in input['authenticationMethodConfigurations'] if 'state' in obj and str(obj['state']).lower() == "enabled"]
         else:
             mfa_enrolled = []
 
         # Filter to keep only auth types that are NOT FIDO or OTP
-        otherAuthTypes = [auth_type for auth_type in mfa_enrolled if auth_type['id'].lower() not in ['fido2', 'microsoftauthenticator']]
+        otherAuthTypes = [auth_type for auth_type in mfa_enrolled if auth_type['id'].lower() not in ['fido2', 'microsoftauthenticator', 'softwareoath']]
 
         # Find the temporaryaccesspass auth_type object
         temp_access_obj = next((auth_type for auth_type in mfa_enrolled if auth_type['id'].lower() == 'temporaryaccesspass'), None)
