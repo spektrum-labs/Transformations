8962a1456ef1106599a55933a5fbc0147914c481
Tim Butler <tim@spektrum.ai>
2026-02-11 09:28:15 -0500

    Addition for Horizon3

---
+++ safeguards/threatintelligence/horizon3/schemas/__init__.py
@@ -0,0 +1,9 @@
+"""Pydantic schemas for transformation inputs."""
+
+from .confirmedlicensepurchased import ConfirmedlicensepurchasedInput
+from .intel_transform import IntelTransformInput
+
+__all__ = [
+    "ConfirmedlicensepurchasedInput",
+    "IntelTransformInput",
+]
