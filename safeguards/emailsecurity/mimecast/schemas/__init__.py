c22c26237cd5b45d633fb93339d3f7850dd6ab5f
Simon Mullaney <159937732+simon-spektrum@users.noreply.github.com>
2026-04-29 13:51:43 +0100

    update: __init__.py

--- safeguards/emailsecurity/mimecast/schemas/__init__.py
+++ safeguards/emailsecurity/mimecast/schemas/__init__.py
@@ -1,13 +1,21 @@
-"""Pydantic schemas for transformation inputs."""
+"""Schema registry for this vendor's transformations."""
 
+from .confirmedLicensePurchased import ConfirmedLicensePurchasedInput
 from .confirmedlicensepurchased import ConfirmedlicensepurchasedInput
-from .isantiphishingenabled import IsantiphishingenabledInput
+from
+from .isDNSConfigured import IsDNSConfiguredInput
+from .isEmailLoggingEnabled import IsEmailLoggingEnabledInput
 from .isdnsconfigured import IsdnsconfiguredInput
-from .isemailloggingenabled import IsemailloggingenabledInput
+from
 from .isurlrewriteenabled import IsurlrewriteenabledInput
 
+__all__
+
 __all__ = [
+    "ConfirmedLicensePurchasedInput",
     "ConfirmedlicensepurchasedInput",
+    "IsDNSConfiguredInput",
+    "IsEmailLoggingEnabledInput",
     "IsantiphishingenabledInput",
     "IsdnsconfiguredInput",
     "IsemailloggingenabledInput",
