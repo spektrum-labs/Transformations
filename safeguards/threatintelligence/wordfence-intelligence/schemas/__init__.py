8bb89eb02ac78c513cafe71f6ca96b1cf0ba0ba4
Simon Mullaney <159937732+simon-spektrum@users.noreply.github.com>
2026-08-27 12:26:22 +0100

    add: __init__.py

---
+++ safeguards/threatintelligence/wordfence-intelligence/schemas/__init__.py
@@ -0,0 +1,7 @@
+"""Schema registry for this vendor's transformations."""
+
+from .remediationGuidanceProvidedPercentage import RemediationGuidanceProvidedPercentageInput
+
+__all__ = [
+    "RemediationGuidanceProvidedPercentageInput",
+]
