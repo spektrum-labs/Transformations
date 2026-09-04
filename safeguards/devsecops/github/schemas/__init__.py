c5e55a9e3bcceb4b35cc7b02ac4b38ddc8c10efc
Simon Mullaney <159937732+simon-spektrum@users.noreply.github.com>
2026-09-04 09:57:22 +0100

    add: __init__.py

---
+++ safeguards/devsecops/github/schemas/__init__.py
@@ -0,0 +1,7 @@
+"""Schema registry for this vendor's transformations."""
+
+from .isDependabotAlertsEnabled import IsDependabotAlertsEnabledInput
+
+__all__ = [
+    "IsDependabotAlertsEnabledInput",
+]
